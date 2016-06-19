1880c7e68971681c26211f742434115ce41fa172
Tanner Nelson <tanner@bluebite.com>
2016-06-19 18:45:39 -0400

    update cmysql

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
-   		.Package(url: "https://github.com/qutheory/cmysql.git", majorVersion: 0, minor: 1),
+   		.Package(url: "https://github.com/qutheory/cmysql.git", majorVersion: 0, minor: 2),
    		.Package(url: "https://github.com/qutheory/libc.git", majorVersion: 0, minor: 1),
     ]
 )
