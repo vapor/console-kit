dbfe494fdbd45b504656b9ffb247ff179b662899
Tanner Nelson <tanner@bluebite.com>
2016-06-19 20:41:09 -0400

    fix linker

--- Tests/MySQL/Utilities.swift
+++ Tests/MySQL/Utilities.swift
@@ -4,21 +4,23 @@ import MySQL
 extension MySQL.Database {
     static func makeTestConnection() -> MySQL.Database {
         do {
-            return try MySQL.Database(
+            let mysql = try MySQL.Database(
                 host: "127.0.0.1",
                 user: "root",
                 password: "",
                 database: "test"
             )
+            try mysql.execute("SELECT @@version")
+            return mysql
         } catch {
             print()
             print()
-            print("⚠️ MySQL Not Configured ⚠️")
+            print("⚠️  MySQL Not Configured ⚠️")
             print()
             print("Error: \(error)")
             print()
             print("You must configure MySQL to run with the following configuration: ")
-            print("    user: 'travis'")
+            print("    user: 'root'")
             print("    password: '' // (empty)")
             print("    host: '127.0.0.1'")
             print("    database: 'test'")
