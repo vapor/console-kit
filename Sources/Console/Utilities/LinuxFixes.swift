2b103a79a0fc271c44370cd4fcf2dd0e3befd653
Tanner Nelson <me@tanner.xyz>
2016-07-07 16:44:40 -0400

    appy more linux fixes

--- Sources/Console/Utilities/LinuxFixes.swift
+++ Sources/Console/Utilities/LinuxFixes.swift
@@ -7,29 +7,29 @@ typealias Task = NSTask
 typealias Data = NSData
 
 extension Data {
-    public var count: Int {
+    var count: Int {
         return self.length
     }
 
-    public func copyBytes(to: UnsafeMutablePointer<Void>, count: availableData.count) {
+    func copyBytes(to: UnsafeMutablePointer<Void>, count: availableData.count) {
         getBytes(bytes, length: count)
     }
 
-    public convenience init(bytes: [UInt8]) {
-        super.init(&bytes, length: bytes.count)
+    convenience init(bytes: [UInt8]) {
+        self.init(&bytes, length: bytes.count)
     }
 }
 
 extension FileHandle {
-    public func standardError() -> FileHandle {
+    static func standardError() -> FileHandle {
         return fileHandleWithStandardError()
     }
 
-    public func standardOutput() -> FileHandle {
+    static func standardOutput() -> FileHandle {
         return fileHandleWithStandardOutput()
     }
 
-    public func standardInput() -> FileHandle {
+    static func standardInput() -> FileHandle {
         return fileHandleWithStandardInput()
     }
 }
