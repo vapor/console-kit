1c38215f1c87edb075573099e4988509c758a062
Tanner Nelson <me@tanner.xyz>
2018-01-22 02:45:40 -0500

    add autocomplete output

--- Sources/Command/Console+Run.swift
+++ Sources/Command/Console+Run.swift
@@ -25,10 +25,10 @@ extension Console {
 
         if let help = try input.parse(option: .flag(name: "help")) {
             assert(help == "true")
-            try outputHelp(
-                for: runnable,
-                executable: input.executablePath.joined(separator: " ")
-            )
+            try outputHelp(for: runnable, executable: input.executablePath.joined(separator: " "))
+        } else if let autocomplete = try input.parse(option: .flag(name: "autocomplete")) {
+            assert(autocomplete == "true")
+            try outputAutocomplete(for: runnable, executable: input.executablePath.joined(separator: " "))
         } else {
             let context = try CommandContext.make(from: &input, console: self, for: runnable)
             try runnable.run(using: context)
