c5a7378c7c571c27c252f5a5222b3f7da4785e85
tanner0101 <me@tanner.xyz>
2018-02-09 19:51:09 -0500

    3.0 deps update

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
     ],
     targets: [
         .target(name: "Console", dependencies: ["Async", "Bits", "COperatingSystem", "Service"]),
