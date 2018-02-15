b78ad7bfd75efafed1e6e4325b575bc1820f6715
Tanner Nelson <me@tanner.xyz>
2018-02-14 20:00:21 -0500

    prerelease semver updates

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/service.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
     ],
     targets: [
         .target(name: "Console", dependencies: ["Async", "Bits", "COperatingSystem", "Service"]),
