9b219b46ce0248924ed014c23bf8d4df05f2ad80
tanner0101 <me@tanner.xyz>
2018-03-21 20:41:21 -0400

    sync log output

--- Sources/Logging/PrintLogger.swift
+++ Sources/Logging/PrintLogger.swift
@@ -1,12 +1,20 @@
+import Dispatch
 import Foundation
 
 /// Simply prints the supplied logs to Swift.print
 public final class PrintLogger: Logger {
+    /// Internal queue for synchronizing log output.
+    private let queue: DispatchQueue
+
     /// Create a new print logger
-    public init() {}
+    public init() {
+        queue = .init(label: "codes.vapor.console.logging.print-logger.sync")
+    }
 
     /// See logger.log
     public func log(_ string: String, at level: LogLevel, file: String, function: String, line: UInt, column: UInt) {
-        Swift.print("[\(level)] \(string) (\(file):\(function):\(line):\(column))")
+        queue.async {
+            Swift.print("[\(level)] \(string) (\(file):\(function):\(line):\(column))")
+        }
     }
 }
