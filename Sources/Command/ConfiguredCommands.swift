3a4abe9b74b87b278f622dbffa19593c96bb53f5
tanner0101 <me@tanner.xyz>
2018-03-27 11:32:14 -0400

    fix docs typo

--- Sources/Command/ConfiguredCommands.swift
+++ Sources/Command/ConfiguredCommands.swift
@@ -1,6 +1,6 @@
 import Service
 
-/// Represents a top-level group of configured commands. This is usually created by calling `resolvle(for:)` on `CommandConfig`.
+/// Represents a top-level group of configured commands. This is usually created by calling `resolve(for:)` on `CommandConfig`.
 public struct ConfiguredCommands: Service {
     /// Top-level available commands, stored by unique name.
     public let commands: [String: CommandRunnable]
