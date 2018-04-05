a76b17cc00f637f0ca6eccb206ef880aea14cc9c
tanner0101 <me@tanner.xyz>
2018-04-04 22:19:37 -0400

    remove Thread.async in favor of method in Core

--- Sources/Console/Utilities/Exports.swift
+++ Sources/Console/Utilities/Exports.swift
@@ -1,13 +1,2 @@
 @_exported import Core
 @_exported import Service
-
-// add to core
-extension Thread {
-    public static func async(work: @escaping () -> Void) {
-        if #available(OSX 10.12, *) {
-            Thread.detachNewThread(work)
-        } else {
-            fatalError("macOS 10.12 or later required")
-        }
-    }
-}
