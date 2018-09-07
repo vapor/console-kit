7702975ee1955d498aef3b7566e54a88cf697bed
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-07 09:45:52 -0500

    Added 'optional' property to CommandArgument struct

--- Sources/Command/Command/CommandArgument.swift
+++ Sources/Command/Command/CommandArgument.swift
@@ -31,19 +31,33 @@ public struct CommandArgument {
     /// - parameters:
     ///     - name: This argument's unique name. Use this to fetch the argument from the `CommandContext`.
     public static func argument(name: String, help: [String] = []) -> CommandArgument {
-        return .init(name: name, help: help)
+        return .init(name: name, optional: false, help: help)
     }
 
+    /// Creates a new `CommandArgument`. The command does not fail if this argument is not passed in.
+    ///
+    ///      let arguments: [CommandArgument] = [.optional(name: "message")]
+    ///
+    /// - parameters:
+    ///     - name: This argument's unique name. Use this to fetch the argument from the `CommandContext`.
+    public static func optional(name: String, help: [String] = []) -> CommandArgument {
+        return .init(name: name, optional: true, help: help)
+    }
+    
     /// The argument's unique name.
     public let name: String
 
     /// The arguments's help text when `--help` is passed.
     public let help: [String]
 
+    /// Whether the command should automatically fail if the argument is passed in or not.
+    public let optional: Bool
+    
     /// Creates a new command argument
     /// Create via static methods.
-    internal init(name: String, help: [String]) {
+    internal init(name: String, optional: Bool, help: [String]) {
         self.name = name
         self.help = help
+        self.optional = optional
     }
 }
