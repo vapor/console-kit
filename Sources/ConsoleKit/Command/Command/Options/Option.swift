47416bdee3cfded6b03caa1e318254ec8c149907
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-15 17:18:05 -0500

    Conformed Option struct to AnyOption protocol

--- Sources/ConsoleKit/Command/Command/Options/Option.swift
+++ Sources/ConsoleKit/Command/Command/Options/Option.swift
@@ -1,8 +1,24 @@
+/// A type-erased `Option`.
+public protocol AnyOption {
+    
+    /// The option's unique name.
+    var name: String { get }
+    
+    /// The option's help text when `--help` is passed in.
+    var help: String? { get }
+    
+    /// The option's short flag.
+    var short: Character? { get }
+    
+    /// The type that the option value gets decoded to.
+    var type: LosslessStringConvertible.Type { get }
+}
+
 /// A supported option for a command.
 ///
 ///     exec command [--opt -o]
 ///
-public struct Option<T> where T: LosslessStringConvertible {
+public struct Option<T>: AnyOption where T: LosslessStringConvertible {
     private let nameInit: () -> String
     private let helpInit: () -> String?
     private let shortInit: () -> Character?
@@ -30,7 +46,7 @@ public struct Option<T> where T: LosslessStringConvertible {
         return self.nameInit()
     }
     
-    /// The option's short flag
+    /// The option's short flag.
     public var short: Character? {
         return self.shortInit()
     }
@@ -38,6 +54,13 @@ public struct Option<T> where T: LosslessStringConvertible {
     /// The option's help text when `--help` is passed in.
     public var help: String? {
         return self.helpInit()
+    }
+    
+    /// The type that the option value gets decoded to.
+    ///
+    /// Required by `AnyOption`.
+    public var type: LosslessStringConvertible.Type {
+        return T.self
     }
 }
 
