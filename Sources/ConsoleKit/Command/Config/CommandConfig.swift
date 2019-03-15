47a5ef29ebc1805d49fbada94b12574b971e54e8
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-15 17:27:18 -0500

    #commentallthethings

--- Sources/ConsoleKit/Command/Config/CommandConfig.swift
+++ Sources/ConsoleKit/Command/Config/CommandConfig.swift
@@ -1,48 +1,48 @@
-#warning("TODO: consider removing separate config struct")
-/// Configures commands for a service container.
-///
-///     var commandConfig = CommandConfig.default()
-///     /// You can register command types that will be lazily created
-///     commandConfig.use(FooCommand.self, as: "foo")
-///     /// You can also register pre-initialized instances of a command
-///     commandConfig.use(barCommand, as: "bar")
-///     services.register(commandConfig)
-///
-public struct CommandConfig {
-    /// Internal storage
-    private var commands: [String: CommandRunnable]
-
-    /// The default runnable
-    private var defaultCommand: String?
-
-    /// Create a new `CommandConfig`.
-    public init() {
-        self.commands = [:]
-    }
-
-    /// Adds a `Command` instance to the config.
-    ///
-    ///     var commandConfig = CommandConfig.default()
-    ///     commandConfig.use(barCommand, as: "bar")
-    ///     services.register(commandConfig)
-    ///
-    /// - parameters:
-    ///     - command: Some `CommandRunnable`. This type will be requested from the service container later.
-    ///     - name: A unique name for running this command.
-    ///     - isDefault: If `true`, this command will be set as the default command to run when none other are specified.
-    ///                  Setting this overrides any previous default commands.
-    public mutating func use(_ command: CommandRunnable, as name: String, isDefault: Bool = false) {
-        commands[name] = command
-        if isDefault {
-            defaultCommand = name
-        }
-    }
-
-    /// Resolves the configured commands to a `ConfiguredCommands` struct.
-    ///
-    /// - returns: `Commands` struct which contains initialized commands.
-    /// - throws: Errors creating the lazy commands from the container.
-    public func resolve() throws -> Commands {
-        return .init(commands: commands, defaultCommand: defaultCommand)
-    }
-}
+//#warning("TODO: consider removing separate config struct")
+///// Configures commands for a service container.
+/////
+/////     var commandConfig = CommandConfig.default()
+/////     /// You can register command types that will be lazily created
+/////     commandConfig.use(FooCommand.self, as: "foo")
+/////     /// You can also register pre-initialized instances of a command
+/////     commandConfig.use(barCommand, as: "bar")
+/////     services.register(commandConfig)
+/////
+//public struct CommandConfig {
+//    /// Internal storage
+//    private var commands: [String: CommandRunnable]
+//
+//    /// The default runnable
+//    private var defaultCommand: String?
+//
+//    /// Create a new `CommandConfig`.
+//    public init() {
+//        self.commands = [:]
+//    }
+//
+//    /// Adds a `Command` instance to the config.
+//    ///
+//    ///     var commandConfig = CommandConfig.default()
+//    ///     commandConfig.use(barCommand, as: "bar")
+//    ///     services.register(commandConfig)
+//    ///
+//    /// - parameters:
+//    ///     - command: Some `CommandRunnable`. This type will be requested from the service container later.
+//    ///     - name: A unique name for running this command.
+//    ///     - isDefault: If `true`, this command will be set as the default command to run when none other are specified.
+//    ///                  Setting this overrides any previous default commands.
+//    public mutating func use(_ command: CommandRunnable, as name: String, isDefault: Bool = false) {
+//        commands[name] = command
+//        if isDefault {
+//            defaultCommand = name
+//        }
+//    }
+//
+//    /// Resolves the configured commands to a `ConfiguredCommands` struct.
+//    ///
+//    /// - returns: `Commands` struct which contains initialized commands.
+//    /// - throws: Errors creating the lazy commands from the container.
+//    public func resolve() throws -> Commands {
+//        return .init(commands: commands, defaultCommand: defaultCommand)
+//    }
+//}
--- Sources/ConsoleKit/Command/Group/BasicCommandGroup.swift
+++ Sources/ConsoleKit/Command/Group/BasicCommandGroup.swift
@@ -1,27 +1,27 @@
-import NIO
-
-/// A basic `CommandGroup` implementation.
-internal struct BasicCommandGroup: CommandGroup {
-    /// See `CommandGroup`.
-    var commands: Commands
-
-    /// See `CommandGroup`.
-    var options: [CommandOption] {
-        return []
-    }
-
-    /// See `CommandGroup`.
-    var help: [String]
-
-    /// Creates a new `BasicCommandGroup`.
-    internal init(commands: Commands, help: [String]) {
-        self.help = help
-        self.commands = commands
-    }
-
-    /// See `CommandGroup`.
-    func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
-        // should never run
-        return context.eventLoop.makeSucceededFuture(())
-    }
-}
+//import NIO
+//
+///// A basic `CommandGroup` implementation.
+//internal struct BasicCommandGroup: CommandGroup {
+//    /// See `CommandGroup`.
+//    var commands: Commands
+//
+//    /// See `CommandGroup`.
+//    var options: [CommandOption] {
+//        return []
+//    }
+//
+//    /// See `CommandGroup`.
+//    var help: [String]
+//
+//    /// Creates a new `BasicCommandGroup`.
+//    internal init(commands: Commands, help: [String]) {
+//        self.help = help
+//        self.commands = commands
+//    }
+//
+//    /// See `CommandGroup`.
+//    func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
+//        // should never run
+//        return context.eventLoop.makeSucceededFuture(())
+//    }
+//}
