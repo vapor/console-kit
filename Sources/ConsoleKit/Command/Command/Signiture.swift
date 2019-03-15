a1095485e83a3f6ae37519a9dd3bee21e72f1e77
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-15 17:18:16 -0500

    Created Inputs protocol

---
+++ Sources/ConsoleKit/Command/Command/Signiture.swift
@@ -0,0 +1,24 @@
+public protocol Inputs { }
+
+extension Inputs {
+    
+    /// Gets all the `Argument` propeties from a `Inputs` struct.
+    ///
+    /// Because the `Argument` struct is generic, we have to type-erase it and use `AnyArgument`.
+    var arguments: [AnyArgument] {
+        return Mirror(reflecting: self).children.compactMap { property -> AnyArgument? in
+            guard let argument = property.value as? AnyArgument else { return nil }
+            return argument
+        }
+    }
+    
+    /// Gets all the `Option` propeties from a `Inputs` struct.
+    ///
+    /// Because the `Option` struct is generic, we have to type-erase it and use `AnyOption`.
+    var options: [AnyOption] {
+        return Mirror(reflecting: self).children.compactMap { property -> AnyOption? in
+            guard let option = property.value as? AnyOption else { return nil }
+            return option
+        }
+    }
+}
