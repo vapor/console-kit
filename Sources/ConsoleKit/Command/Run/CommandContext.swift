5f70822de0ce94538cf08281666b4214566cab85
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-18 18:18:46 -0500

    Re-implemented CommandContext argument and option methods

--- Sources/ConsoleKit/Command/Run/CommandContext.swift
+++ Sources/ConsoleKit/Command/Run/CommandContext.swift
@@ -1,60 +1,86 @@
-///// Contains required data for running a command such as the `Console` and `CommandInput`.
-/////
-///// See `CommandRunnable` for more information.
-//public struct CommandContext {
-//    /// The `Console` this command was run on.
-//    public var console: Console
-//
-//    /// The parsed arguments (according to declared signature).
-//    public var arguments: [String: String]
-//
-//    /// The parsed options (according to declared signature).
-//    public var options: [String: String]
-//    
-//    public let eventLoop: EventLoop
-//
-//    /// Create a new `CommandContext`.
-//    public init(
-//        console: Console,
-//        arguments: [String: String],
-//        options: [String: String]
-//    ) {
-//        self.console = console
-//        self.arguments = arguments
-//        self.options = options
-//        self.eventLoop = console.eventLoopGroup.next()
-//    }
-//
-//    /// Requires an option, returning the value or throwing.
-//    ///
-//    ///     let option = try context.requireOption("foo")
-//    ///
-//    /// Use `.options` to access in a non-required manner.
-//    ///
-//    /// - parameters:
-//    ///     - name: Name of the `CommandOption` to fetch.
-//    public func requireOption(_ name: String) throws -> String {
-//        guard let value = options[name] else {
-//            throw CommandError(identifier: "optionRequired", reason: "Option `\(name)` is required.")
-//        }
-//
-//        return value
-//    }
-//
-//    /// Accesses an argument by name. This will only throw if
-//    /// the argument was not properly declared in your signature.
-//    ///
-//    ///     let arg = try context.argument("message")
-//    ///
-//    /// - parameters:
-//    ///     - name: Name of the `CommandArgument` to fetch.
-//    public func argument(_ name: String) throws -> String {
-//        guard let value = arguments[name] else {
-//            throw CommandError(identifier: "argumentRequired", reason: "Argument `\(name)` is required.")
-//        }
-//        return value
-//    }
-//
+/// Contains required data for running a command such as the `Console` and `CommandInput`.
+///
+/// See `CommandRunnable` for more information.
+public struct CommandContext<Signiture> where Signiture: Inputs {
+    /// The `Console` this command was run on.
+    public var console: Console
+
+    /// The parsed arguments (according to declared signature).
+    public var arguments: [String: String]
+
+    /// The parsed options (according to declared signature).
+    public var options: [String: String]
+    
+    public let eventLoop: EventLoop
+
+    private let signiture: Signiture
+    
+    /// Create a new `CommandContext`.
+    public init(
+        console: Console,
+        arguments: [String: String],
+        options: [String: String]
+    ) {
+        self.console = console
+        self.arguments = arguments
+        self.options = options
+        self.eventLoop = console.eventLoopGroup.next()
+        self.signiture = Signiture()
+    }
+
+    /// Gets an option passed into the command.
+    ///
+    /// If the option was not passed in and no default value was registered, then `nil` is returned.
+    ///
+    ///     let option = try context.option(\.foo)
+    ///
+    /// - parameters:
+    ///   - path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
+    public func option<T>(_ path: KeyPath<Signiture, Option<T>>)throws -> T? where T: LosslessStringConvertible {
+        guard let raw = self.options[self.signiture[keyPath: path].name] else {
+            return nil
+        }
+        guard let value = T.init(raw) else {
+            throw CommandError(identifier: "typeMismatch", reason: "Unable to convert `\(raw)` to type `\(T.self)`")
+        }
+        return value
+    }
+    
+    /// Requires an option, returning the value or throwing.
+    ///
+    ///     let option = try context.requireOption(\.foo)
+    ///
+    /// Use `.option(_:)` to access in a non-required manner.
+    ///
+    /// - parameters:
+    ///   - path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
+    public func requireOption<T>(_ path: KeyPath<Signiture, Option<T>>) throws -> T where T: LosslessStringConvertible {
+        guard let option = try self.option(path) else {
+            let name = self.signiture[keyPath: path].name
+            throw CommandError(identifier: "optionRequired", reason: "Option `\(name)` is required.")
+        }
+
+        return option
+    }
+
+    /// Accesses an argument by name. This will only throw if
+    /// the argument was not properly declared in your signature.
+    ///
+    ///     let message = try context.argument(\.message)
+    ///
+    /// - parameters:
+    ///   - path: The key-path of an `Argument` in the parent command's `Signiture` to fetch.
+    public func argument<T>(_ path: KeyPath<Signiture, Argument<T>>) throws -> T where T: LosslessStringConvertible {
+        let name = self.signiture[keyPath: path].name
+        guard let raw = arguments[name] else {
+            throw CommandError(identifier: "argumentRequired", reason: "Argument `\(name)` is required.")
+        }
+        guard let value = T.init(raw) else {
+            throw CommandError(identifier: "typeMismatch", reason: "Unable to convert `\(raw)` to type `\(T.self)`")
+        }
+        return value
+    }
+
 //    /// Creates a CommandContext, parsing the values from the supplied CommandInput.
 //    static func make(
 //        from input: inout CommandInput,
@@ -98,4 +124,4 @@
 //            options: parsedOptions
 //        )
 //    }
-//}
+}
