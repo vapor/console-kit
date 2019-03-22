042dad802e246276aee4a69e0a5a0143ad77ef5e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-22 11:23:37 -0500

    Re-wrote test commands to use the new Command syntax

--- Tests/ConsoleKitTests/Utilities.swift
+++ Tests/ConsoleKitTests/Utilities.swift
@@ -4,19 +4,19 @@ import NIO
 extension String: Error {}
 
 final class TestGroup: CommandGroup {
+    struct Signature: Inputs {
+        let version = Option<Bool>.value(name: "version", help: "Prints the version")
+    }
+    
     let commands: Commands = [
         "test": TestCommand(),
         "sub": SubGroup()
     ]
 
-    let options: [CommandOption] = [
-        .value(name: "version", help: ["Prints the version"])
-    ]
+    let help: String? = "This is a test grouping!"
 
-    let help = ["This is a test grouping!"]
-
-    func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
-        if context.options["version"] == "true" {
+    func run(using context: CommandContext<TestGroup>) throws -> EventLoopFuture<Void> {
+        if try context.option(\.version) ?? false {
             context.console.print("v2.0")
         } else {
             throw "unknown"
@@ -26,18 +26,18 @@ final class TestGroup: CommandGroup {
 }
 
 final class SubGroup: CommandGroup {
+    struct Signature: Inputs {
+        let version = Option<Bool>.value(name: "version", help: "Prints the version")
+    }
+    
     let commands: Commands = [
         "test": TestCommand()
     ]
 
-    let options: [CommandOption] = [
-        .value(name: "version", help: ["Prints the version"])
-    ]
+    let help: String? = "This is a test sub grouping!"
 
-    let help = ["This is a test sub grouping!"]
-
-    func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
-        if context.options["version"] == "true" {
+    func run(using context: CommandContext<SubGroup>) throws -> EventLoopFuture<Void> {
+        if try context.option(\.version) ?? false {
             context.console.print("v2.0")
         } else {
             throw "unknown"
@@ -47,22 +47,23 @@ final class SubGroup: CommandGroup {
 }
 
 final class TestCommand: Command {
-    let arguments: [CommandArgument] = [
-        .argument(
-            name: "foo",
-            help: ["A foo is required", "An error will occur if none exists"]
-        )
-    ]
+    struct Signature: Inputs {
+        let foo = Argument<String>(name: "foo", help: """
+        A foo is required
+        An error will occur if none exists
+        """)
+        
+        let bar = Option<String>.value(name: "bar", short: "b", help: """
+        Add a bar if you so desire
+        Try passing it
+        """)
+    }
 
-    let options: [CommandOption] = [
-        .value(name: "bar", short: "b", help: ["Add a bar if you so desire", "Try passing it"])
-    ]
+    let help: String? = "This is a test command"
 
-    let help = ["This is a test command"]
-
-    func run(using context: CommandContext) throws -> EventLoopFuture<Void> {
-        let foo = try context.argument("foo")
-        let bar = try context.requireOption("bar")
+    func run(using context: CommandContext<TestCommand>) throws -> EventLoopFuture<Void> {
+        let foo = try context.argument(\.foo)
+        let bar = try context.requireOption(\.bar)
         context.console.output("Foo: \(foo) Bar: \(bar)".consoleText(.info))
         return context.eventLoop.makeSucceededFuture(())
     }
