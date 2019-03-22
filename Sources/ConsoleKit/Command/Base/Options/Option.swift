df866318dbd078ae5b61fb8c781ca9d80927932a
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-22 10:04:09 -0500

    Added .optionType property to AnyOption protocol and Option<Struct>

--- Sources/ConsoleKit/Command/Base/Options/Option.swift
+++ Sources/ConsoleKit/Command/Base/Options/Option.swift
@@ -9,6 +9,9 @@ public protocol AnyOption {
     /// The option's short flag.
     var short: Character? { get }
     
+    /// The type of command option that the instance represents.
+    var optionType: OptionType { get }
+    
     /// The type that the option value gets decoded to.
     var type: LosslessStringConvertible.Type { get }
 }
@@ -27,6 +30,17 @@ public struct Option<T>: AnyOption where T: LosslessStringConvertible {
     /// The option's help text when `--help` is passed in.
     public let short: Character?
     
+    /// The type of command option that the instance represents.
+    ///
+    /// The option could be a flag:
+    ///
+    ///     exec command -f
+    ///
+    /// Of a value:
+    ///
+    ///     exec command -v foo
+    public let optionType: OptionType
+    
     /// The type that the option value gets decoded to.
     ///
     /// Required by `AnyOption`.
@@ -42,14 +56,63 @@ public struct Option<T>: AnyOption where T: LosslessStringConvertible {
     ///   - name: The option's unique name. Use this to get the option value from the `CommandContext`.
     ///   - short: The short-hand for the flag that can be passed in to the command call.
     ///   - help: The option's help text when `--help` is passed in.
+    ///   - type: The type of command option. This can be a `flag` or `value`.
     public init(
         name: String,
         short: Character? = nil,
-        help: String? = nil
+        help: String? = nil,
+        type: OptionType = .flag
     ) {
         self.name = name
         self.short = short
         self.help = help
+        self.optionType = type
+    }
+    
+    /// Creates an `Option` instance with the `optionType` as `.value`
+    ///
+    ///     Option<Int>.value(name: "result", short: "r", help: "The number of results to fetch", default: 100)
+    ///
+    /// - Parameters:
+    ///   - name: The option's unique name. Use this to get the option value from the `CommandContext`.
+    ///   - short: The short-hand for the flag that can be passed in to the command call.
+    ///   - help: The option's help text when `--help` is passed in.
+    ///   - default: The default value for the option. Defaults to `nil`.
+    ///
+    /// - Returns: A `.value` type `Option` instance created with the values passed in.
+    public static func value<T>(
+        name: String,
+        short: Character? = nil,
+        help: String? = nil,
+        default: T? = nil
+    ) -> Option<T> {
+        return Option<T>(name: name, short: short, help: help, type: .value(default: `default`?.description))
+    }
+    
+    /// Creates an `Option` instance with the `optionType` as `.flat`
+    ///
+    ///     Option<Bool>.value(name: "verbose", short: "v", help: "Output debug logs")
+    ///
+    /// - Parameters:
+    ///   - name: The option's unique name. Use this to get the option value from the `CommandContext`.
+    ///   - short: The short-hand for the flag that can be passed in to the command call.
+    ///   - help: The option's help text when `--help` is passed in.
+    ///
+    /// - Returns: A `.flag` type `Option` instance created with the values passed in.
+    public static func flag<T>(name: String, short: Character? = nil, help: String? = nil) -> Option<T> {
+        return Option<T>(name: name, short: short, help: help, type: .flag)
     }
 }
 
+public enum OptionType {
+    /// Normal option. Requires a value if supplied and there is no default.
+    ///
+    ///     --branch beta
+    ///
+    case value(default: String?)
+    /// Flag option. Does not support a value. If supplied, the value is true.
+    ///
+    ///     --xcode
+    ///
+    case flag
+}
