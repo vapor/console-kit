e13f7d0d6ca30eb8b8e0882da48a4ea360a60271
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-22 10:23:35 -0500

    Re-implemented Console.outputAutocomplete(for:executable:) with new CommandRunnable

--- Sources/ConsoleKit/Command/Run/Output+Autocomplete.swift
+++ Sources/ConsoleKit/Command/Run/Output+Autocomplete.swift
@@ -1,12 +1,15 @@
-//extension Console {
-//    /// Outputs autocomplete data for a supplied `CommandRunnable`.
-//    internal func outputAutocomplete(for runnable: CommandRunnable, executable: String) throws {
-//        var autocomplete: [String] = []
-//        switch runnable.type {
-//        case .command(let arguments): autocomplete += arguments.map { $0.name }
-//        case .group(let commands): autocomplete += commands.commands.keys
-//        }
-//        autocomplete += runnable.options.map { "--" + $0.name }
-//        output(autocomplete.joined(separator: " "), style: .plain)
-//    }
-//}
+extension Console {
+    /// Outputs autocomplete data for a supplied `CommandRunnable`.
+    internal func outputAutocomplete<Runnable>(
+        for runnable: Runnable,
+        executable: String
+    ) throws where Runnable: CommandRunnable {
+        var autocomplete: [String] = []
+        switch runnable.type {
+        case .command(let arguments): autocomplete += arguments.map { $0.name }
+        case .group(let commands): autocomplete += commands.commands.keys
+        }
+        autocomplete += Runnable.signature.options.map { "--" + $0.name }
+        output(autocomplete.joined(separator: " "), style: .plain)
+    }
+}
