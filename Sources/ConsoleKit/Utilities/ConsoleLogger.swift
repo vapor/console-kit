2c8ab064fa8c40007c6660803656b814e01cdaa6
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-22 14:24:40 -0500

    Re-implemented ConsoleLogger using the SSWG Logging API

--- Sources/ConsoleKit/Utilities/ConsoleLogger.swift
+++ Sources/ConsoleKit/Utilities/ConsoleLogger.swift
@@ -1,36 +1,79 @@
-#warning("TODO: update to SSWG logging library")
-//import Logging
-//
-///// Outputs logs to a `Console`.
-//public final class ConsoleLogger: Logger {
-//    /// The `Console` logs will be outputted to.
-//    public let console: Console
-//
-//    /// Create a new `ConsoleLogger`.
-//    public init(console: Console) {
-//        self.console = console
-//    }
-//
-//    /// See `Logger`.
-//    public func log(_ string: String, at level: LogLevel, file: String, function: String, line: UInt, column: UInt) {
-//        let text: ConsoleText = ""
-//            + "[ \(level) ]".consoleText(level.style)
-//            + " "
-//            + string.consoleText()
-//            + " "
-//            + "(\(file):\(line))".consoleText(.info)
-//        console.output(text)
-//    }
-//}
-//
-//extension LogLevel {
-//    /// Converts log level to console style
-//    fileprivate var style: ConsoleStyle {
-//        switch self {
-//        case .custom, .verbose, .debug: return .plain
-//        case .error, .fatal: return .error
-//        case .info: return .info
-//        case .warning: return .warning
-//        }
-//    }
-//}
+import Logging
+
+/// Outputs logs to a `Console`.
+public struct ConsoleLogger: LogHandler {
+    
+    /// See `LogHandler.metadata`.
+    public var metadata: Logger.Metadata
+    
+    /// See `LogHandler.logLevel`.
+    public var logLevel: Logger.Level
+    
+    /// The conosle that the messages will get logged to.
+    public let console: Console
+    
+    /// Creates a new `ConsoleLogger` instance.
+    ///
+    /// - Parameters:
+    ///   - console: The console to log the messages to.
+    ///   - level: The minimum level of message that the logger will output. This defaults to `.debug`, the lowest level.
+    ///   - metadata: Extra metadata to log with the message. This defaults to an empty dictionary.
+    public init(console: Console, level: Logger.Level = .debug, metadata: Logger.Metadata = [:]) {
+        self.metadata = metadata
+        self.logLevel = level
+        self.console = console
+    }
+    
+    /// See `LogHandler[metadataKey:]`.
+    ///
+    /// This just acts as a getter/setter for the `.metadata` property.
+    public subscript(metadataKey key: String) -> Logger.Metadata.Value? {
+        get { return self.metadata[key] }
+        set { self.metadata[key] = newValue }
+    }
+    
+    /// See `LogHandler.log(level:message:metadata:file:function:line:)`.
+    public func log(
+        level: Logger.Level,
+        message: Logger.Message,
+        metadata: Logger.Metadata?,
+        file: String,
+        function: String,
+        line: UInt
+    ) {
+        let text: ConsoleText =
+            "[ \(level.name) ]".consoleText(level.style) +
+            " " +
+            message.description.consoleText() +
+            " " +
+            "(\(file):\(line))".consoleText(.info)
+        
+        console.output(text)
+    }
+}
+
+extension Logger.Level {
+    /// Converts log level to console style
+    fileprivate var style: ConsoleStyle {
+        switch self {
+        case .debug: return .plain
+        case .info, .notice: return .info
+        case .warning: return .warning
+        case .error, .critical: return .error
+        case .alert, .emergency: return ConsoleStyle(color: .brightRed)
+        }
+    }
+    
+    fileprivate var name: String {
+        switch self {
+        case .debug: return "Debug"
+        case .info: return "Info"
+        case .notice: return "Notice"
+        case .warning: return "Warning"
+        case .error: return "Error"
+        case .critical: return "Critical"
+        case .alert: return "Alert"
+        case .emergency: return "Emergency"
+        }
+    }
+}
