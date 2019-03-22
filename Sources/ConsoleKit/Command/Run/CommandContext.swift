7aa5932d4f0af384d557e046d7e58bdc65be2d61
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-22 10:59:02 -0500

    Created type-erased AnyCommandContext struct

--- Sources/ConsoleKit/Command/Run/CommandContext.swift
+++ Sources/ConsoleKit/Command/Run/CommandContext.swift
@@ -1,3 +1,88 @@
+/// A type-erased `CommandContext`
+public struct AnyCommandContext {
+    /// The `Console` this command was run on.
+    public var console: Console
+    
+    /// The parsed arguments (according to declared signature).
+    public var arguments: [String: String]
+    
+    /// The parsed options (according to declared signature).
+    public var options: [String: String]
+    
+    /// Create a new `AnyCommandContext`.
+    public init(
+        console: Console,
+        arguments: [String: String],
+        options: [String: String]
+    ) {
+        self.console = console
+        self.arguments = arguments
+        self.options = options
+    }
+    
+    /// Creates an instance of `CommandContext<Command>` with the data from the current `AnyCommandContext` instance.
+    ///
+    /// - Parameter command: The `Command` generic type for the `CommandContext` instance.
+    /// - Returns: A `CommandContext` with the `console`, `arguments`, and `options` from `AnyCommandContext`.
+    public func context<Command>(command: Command.Type = Command.self) -> CommandContext<Command>
+        where Command: CommandRunnable
+    {
+        return CommandContext<Command>.init(console: self.console, arguments: self.arguments, options: self.options)
+    }
+    
+    /// Creates an `AnyCommandContext`, parsing the values from the supplied `CommandInput`.
+    ///
+    /// - Parameters:
+    ///   - input: The input from a command call that goes into the context's `argument` and `options`.
+    ///   - console: The console instance used by the command.
+    ///   - runnable: The command that will use the context.
+    ///
+    /// - Returns: The context for a command to run.
+    static func make(
+        from input: inout CommandInput,
+        console: Console,
+        for runnable: AnyCommandRunnable
+    ) throws -> AnyCommandContext {
+        var parsedArguments: [String: String] = [:]
+        var parsedOptions: [String: String] = [:]
+        let runnableType = type(of: runnable)
+        
+        for opt in runnableType.inputs.options {
+            parsedOptions[opt.name] = try input.parse(option: opt)
+        }
+        
+        let arguments: [AnyArgument]
+        switch runnable.type {
+        case .command(let a): arguments = a
+        case .group: arguments = []
+        }
+        
+        for arg in arguments {
+            guard let value = try input.parse(argument: arg) else {
+                throw CommandError(
+                    identifier: "argumentRequired",
+                    reason: "Argument `\(arg.name)` is required."
+                )
+            }
+            parsedArguments[arg.name] = value
+        }
+        
+        
+        guard input.arguments.count == 0 else {
+            throw CommandError(
+                identifier: "excessInput",
+                reason: "Too many arguments or unsupported options were supplied: \(input.arguments)"
+            )
+        }
+        
+        return AnyCommandContext(
+            console: console,
+            arguments: parsedArguments,
+            options: parsedOptions
+        )
+    }
+}
+
 /// Contains required data for running a command such as the `Console` and `CommandInput`.
 ///
 /// See `CommandRunnable` for more information.
@@ -31,8 +116,7 @@ public struct CommandContext<Command> where Command: CommandRunnable {
     ///
     ///     let option = try context.option(\.foo)
     ///
-    /// - parameters:
-    ///   - path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
+    /// - Parameter path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
     public func option<T>(_ path: KeyPath<Command.Signature, Option<T>>)throws -> T?
         where T: LosslessStringConvertible
     {
@@ -51,8 +135,7 @@ public struct CommandContext<Command> where Command: CommandRunnable {
     ///
     /// Use `.option(_:)` to access in a non-required manner.
     ///
-    /// - parameters:
-    ///   - path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
+    /// - Parameter path: The key-path of an `Option` in the parent command's `Signiture` to fetch.
     public func requireOption<T>(_ path: KeyPath<Command.Signature, Option<T>>) throws -> T
         where T: LosslessStringConvertible
     {
@@ -69,8 +152,7 @@ public struct CommandContext<Command> where Command: CommandRunnable {
     ///
     ///     let message = try context.argument(\.message)
     ///
-    /// - parameters:
-    ///   - path: The key-path of an `Argument` in the parent command's `Signiture` to fetch.
+    /// - Parameter path: The key-path of an `Argument` in the parent command's `Signiture` to fetch.
     public func argument<T>(_ path: KeyPath<Command.Signature, Argument<T>>) throws -> T
         where T: LosslessStringConvertible
     {
@@ -84,47 +166,12 @@ public struct CommandContext<Command> where Command: CommandRunnable {
         return value
     }
 
-    /// Creates a CommandContext, parsing the values from the supplied CommandInput.
+    /// See `AnyCommandContext.make(from:console:for:)`.
     static func make<Runnable>(
         from input: inout CommandInput,
         console: Console,
         for runnable: Runnable
     ) throws -> CommandContext<Runnable> where Runnable: CommandRunnable {
-        var parsedArguments: [String: String] = [:]
-        var parsedOptions: [String: String] = [:]
-
-        for opt in Runnable.signature.options {
-            parsedOptions[opt.name] = try input.parse(option: opt)
-        }
-
-        let arguments: [AnyArgument]
-        switch runnable.type {
-        case .command(let a): arguments = a
-        case .group: arguments = []
-        }
-
-        for arg in arguments {
-            guard let value = try input.parse(argument: arg) else {
-                throw CommandError(
-                    identifier: "argumentRequired",
-                    reason: "Argument `\(arg.name)` is required."
-                )
-            }
-            parsedArguments[arg.name] = value
-        }
-
-
-        guard input.arguments.count == 0 else {
-            throw CommandError(
-                identifier: "excessInput",
-                reason: "Too many arguments or unsupported options were supplied: \(input.arguments)"
-            )
-        }
-
-        return CommandContext<Runnable>(
-            console: console,
-            arguments: parsedArguments,
-            options: parsedOptions
-        )
+        return try AnyCommandContext.make(from: &input, console: console, for: runnable).context(command: Runnable.self)
     }
 }
