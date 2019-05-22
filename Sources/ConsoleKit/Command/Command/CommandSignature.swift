d4ac9ad2ecfde2c1b7c15697f5024f419e236008
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-22 18:19:45 -0500

    Created CommandSignature.validate(inputs:) method

--- Sources/ConsoleKit/Command/Command/CommandSignature.swift
+++ Sources/ConsoleKit/Command/Command/CommandSignature.swift
@@ -26,4 +26,39 @@ extension CommandSignature {
             return option
         }
     }
+
+    /// Verifies that the input for the command can be properly mapped to the commands signature (arguments and options).
+    ///
+    /// - Parameter input: The command input to verify
+    /// - Returns: The input passed in. The returned value is discarable if you don't need it.
+    /// - Throws:
+    ///   - `missingArgument` if an argument value was not found in the input.
+    ///   - `badInputType` if an argument's or option's value cannot be converted to the expected Swift type.
+    @discardableResult
+    func validate(input: [String: String])throws -> [String: String] {
+        try self.arguments.forEach { argument in
+            guard let value = input[argument.name] else {
+                throw CommandError(identifier: "missingArgument", reason: "Missing expected argument `\(argument.name)`")
+            }
+            guard argument.type.init(value) != nil else {
+                throw CommandError(
+                    identifier: "badInputType",
+                    reason: "Value for argument `\(argument.name)` must be convertable to type `\(argument.type)`"
+                )
+            }
+        }
+
+        try self.options.forEach { option in
+            if let value = input[option.name] {
+                guard option.valueType.init(value) != nil else {
+                    throw CommandError(
+                        identifier: "badInputType",
+                        reason: "Value for option `\(option.name)` must be convertable to type `\(option.valueType)`"
+                    )
+                }
+            }
+        }
+
+        return input
+    }
 }
