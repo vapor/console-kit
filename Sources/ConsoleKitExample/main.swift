1e75048bcf12a73d7484002ebbed4401bf7e678a
Kyle LeNeau <k.leneau@gmail.com>
2020-01-19 11:46:58 -0800

    Fixing Tests and updating the example

--- Sources/ConsoleKitExample/main.swift
+++ Sources/ConsoleKitExample/main.swift
@@ -3,14 +3,15 @@ import Foundation
 
 let console: Console = Terminal()
 var input = CommandInput(arguments: CommandLine.arguments)
+var context = CommandContext(console: console, input: input)
 
-var config = CommandConfiguration()
-config.use(DemoCommand(), as: "demo", isDefault: true)
+var commands = Commands()
+commands.use(DemoCommand(), as: "demo", isDefault: true)
 
 do {
-    let commands = try config.resolve()
+    let group = commands
         .group(help: "An example command-line application built with ConsoleKit")
-    try console.run(commands, input: input)
+    try console.run(group, input: input)
 } catch let error {
     console.error(error.localizedDescription)
     exit(1)
