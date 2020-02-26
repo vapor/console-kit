1064b2a1a0fa848f99321e55d746f189d91e8269
Max Liberman <liberman@wetransfer.com>
2020-02-26 11:26:51 -0500

    Add `--output` Option

--- Sources/ConsoleKit/Utilities/GenerateAutocompleteCommand.swift
+++ Sources/ConsoleKit/Utilities/GenerateAutocompleteCommand.swift
@@ -12,20 +12,50 @@ struct GenerateAutocompleteCommand: Command {
 
     struct Signature: CommandSignature {
 
-        @Argument(
+        @Option(
             name: "shell",
-            help: "The shell to target (\(Shell.allCases.map { "\($0)" }.joined(separator: "|")))",
+            short: "s",
+            help: "Generate completion script for SHELL [ \(Shell.allCases.map { "\($0)" }.joined(separator: " | ")) ]. \n"
+                + "Defaults to the \"SHELL\" environment variable if possible."
+            ,
             completion: .values(of: Shell.self)
         )
-        var shell: Shell
+        var shell: Shell?
+
+        @Option(
+            name: "output",
+            short: "o",
+            help: "Write output to file at OUTPUT. \n"
+                + "Defaults to printing to stdout.",
+            completion: .files()
+        )
+        var output: String?
     }
 
     func run(using context: CommandContext, signature: Signature) throws {
         guard let rootCommand = self.rootCommand else { fatalError("`rootCommand` was not initialized") }
+        guard let shell = signature.shell ?? self.environmentShell() else {
+            throw CommandError.missingRequiredArgument(signature.$shell.name)
+        }
         // Reset the executable path
         var context = context
         context.input.executablePath = [context.input.executablePath.first!]
-        let script = rootCommand.renderCompletionScript(using: context, shell: signature.shell)
-        context.console.output(script, style: .plain)
+        let script = rootCommand.renderCompletionScript(using: context, shell: shell)
+        if let output = signature.output, let contents = script.data(using: .utf8) {
+            try contents.write(to: URL(fileURLWithPath: output))
+            context.console.info("\n\(shell) completion script written to \(output)")
+        } else {
+            context.console.output(script, style: .plain)
+        }
+    }
+
+    /// Returns a `Shell` value created from the process's environment variable `"SHELL"`.
+    private func environmentShell() -> Shell? {
+        guard
+            let shellPath = ProcessInfo.processInfo.environment["SHELL"],
+            let shellName = shellPath.split(separator: "/").last,
+            let shell = Shell(rawValue: String(shellName))
+            else { return nil }
+        return shell
     }
 }
