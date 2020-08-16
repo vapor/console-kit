99e689279587e723ca2853e3e9aa79484c542a3a
Gwynne Raskind <gwynne@darkrainfall.org>
2020-08-16 09:54:54 -0500

    Rewrite log message rendering of ConsoleLogger to leverage the outputConfiguration and styleConfiguration. The functionality for filtering labels and regular expressions is not yet implemented in this version.

--- Sources/ConsoleKit/Logging/ConsoleLogger.swift
+++ Sources/ConsoleKit/Logging/ConsoleLogger.swift
@@ -1,4 +1,6 @@
 import Logging
+import class Foundation.ISO8601DateFormatter
+import struct Foundation.Date
 
 /// Outputs logs to a `Console`.
 public struct ConsoleLogger: LogHandler {
@@ -74,28 +76,56 @@ public struct ConsoleLogger: LogHandler {
         function: String,
         line: UInt
     ) {
+        /// - TODO: Preallocate a medium-sized buffer that can be reused for building log messages instead of glomming ConsoleTexts together.
         var text: ConsoleText = ""
+        
+        // Note: The use of `" ".consoleText(self.styleConfiguration.baseStyle)` and similar constructs seems pointless at
+        //       first glance, but its necessity becomes immediately obvious after, for example, giving each of the styles
+        //       a different (and non-default) background color.
+        // TODO: Don't torment the construction of the consoleText pieces so much. This can be done ENORMOUSLY more efficiently.
 
-        if self.logLevel <= .trace {
-            text += "[ \(self.label) ] ".consoleText()
+        // Render the timestamp if currently visible.
+        if self.outputConfiguration.timestampDisplayLogLevel.map({ $0 >= self.logLevel }) ?? false {
+            text += "[".consoleText(self.styleConfiguration.baseStyle) +
+                    "\(self.outputConfiguration.timestampLoggingFormat.render(at: Date()))"
+                        .consoleText(self.styleConfiguration.timestampStyle ?? self.styleConfiguration.baseStyle) +
+                    "] ".consoleText(self.styleConfiguration.baseStyle)
         }
 
-        text += "[ \(level.name) ]".consoleText(level.style)
-            + " "
-            + message.description.consoleText()
-
-        let allMetadata = (metadata ?? [:]).merging(self.metadata) { (a, _) in a }
-
-        if !allMetadata.isEmpty {
-            // only log metadata if not empty
-            text += " " + allMetadata.sortedDescriptionWithoutQuotes.consoleText()
+        // Render the label if currently visible.
+        if self.outputConfiguration.labelDisplayLogLevel.map({ $0 >= self.logLevel }) ?? false {
+            text += "[ ".consoleText(self.styleConfiguration.baseStyle) +
+                    "\(self.label)".consoleText(self.styleConfiguration.labelStyle ?? self.styleConfiguration.baseStyle) +
+                    " ] ".consoleText(self.styleConfiguration.baseStyle)
         }
 
-        // log file info if we are debug or lower
-        if self.logLevel <= .debug {
-            // log the concise path + line
-            let fileInfo = self.conciseSourcePath(file) + ":" + line.description
-            text += " (" + fileInfo.consoleText() + ")"
+        // Render the level and message (always visible).
+        text += "[ ".consoleText(self.styleConfiguration.baseStyle) +
+                "\(level.rawValue.uppercased())".consoleText(self.styleConfiguration.levelStyles[level] ?? self.styleConfiguration.baseStyle) +
+                " ]".consoleText(self.styleConfiguration.baseStyle) +
+                " ".consoleText(self.styleConfiguration.baseStyle) +
+                "\(message)".consoleText(self.styleConfiguration.messageStyle ?? self.styleConfiguration.baseStyle)
+
+        // Render the metadata if currently visible _and_ nonempty.
+        if self.outputConfiguration.metadataDisplayLogLevel.map({ $0 >= self.logLevel }) ?? false,
+           let allMetadata = self.mergeMetadata(with: metadata)
+        {
+            text += " [".consoleText(self.styleConfiguration.baseStyle) +
+            // TODO: In the overwhelmingly common case where the metadata is just `self.metadata`, the sorting and rendering of
+            // the description can be cached in `subscript(metdata:).setter` instead of repeated every time a message is logged.
+                    "\(allMetadata.sorted(by: { $0.key < $1.key }).map { "\($0.description): \($1)" }.joined(separator: ", "))"
+                        .consoleText(self.styleConfiguration.metadataStyle ?? self.styleConfiguration.baseStyle) +
+                    "]".consoleText(self.styleConfiguration.baseStyle)
+        }
+        
+        // Render the source file and line if currently visible.
+        if self.outputConfiguration.sourceFileLineDisplayLogLevel.map({ $0 >= self.logLevel }) ?? false {
+            // TODO: When possible to do so safely, use `source` instead of hacking up `file`.
+            text += " (".consoleText(self.styleConfiguration.baseStyle) +
+                    "\(self.conciseSourcePath(file))".consoleText(self.styleConfiguration.fileStyle ?? self.styleConfiguration.baseStyle) +
+                    ":".consoleText(self.styleConfiguration.baseStyle) +
+                    "\(line.description)".consoleText(self.styleConfiguration.lineStyle ?? self.styleConfiguration.baseStyle) +
+                    ")".consoleText(self.styleConfiguration.baseStyle)
         }
 
         self.console.output(text)
@@ -116,13 +146,31 @@ public struct ConsoleLogger: LogHandler {
     }
 }
 
-private extension Logger.Metadata {
-    var sortedDescriptionWithoutQuotes: String {
-        let contents = Array(self)
-            .sorted(by: { $0.0 < $1.0 })
-            .map { "\($0.description): \($1)" }
-            .joined(separator: ", ")
-        return "[\(contents)]"
+private extension LogHandler {
+    /// Merge the metadata provided as "one-off" values for a single log message with the `LogHandler`'s "persistent"
+    /// metadata, according to the following rules:
+    ///
+    /// - If no message-specific metadata was provided at all (a `nil` input), the result is identical to an empty
+    ///   input (i.e. `[:]`).
+    ///
+    /// - If a message-specific metadata key conflicts with a persistent metadata key, only the message-specific value
+    ///   appears in the result.
+    ///
+    /// - If both the message-specific metadata and the persistent metadata are empty or missing (i.e. if the result
+    ///   would be empty), `nil` is returned in lieu of an empty dictionary.
+    ///
+    /// Typical usage:
+    ///
+    ///     if let allMetadata = self.mergeMetadata(with: messageMetadata) {
+    ///         // include the metadata in the log message
+    ///     }
+    func mergeMetadata(with oneOffMetadata: Logger.Metadata?) -> Logger.Metadata? {
+        if let oneOffMetadata = oneOffMetadata, !oneOffMetadata.isEmpty {
+            guard !self.metadata.isEmpty else { return oneOffMetadata }
+            return self.metadata.merging(oneOffMetadata) { $1 }
+        } else {
+            return self.metadata.isEmpty ? nil : self.metadata
+        }
     }
 }
 
@@ -148,6 +196,27 @@ extension LoggingSystem {
     ) {
         self.bootstrap { label in
             return ConsoleLogger(label: label, console: console, level: level, metadata: metadata, settings: outputConfiguration, style: styleConfiguration)
+        }
+    }
+}
+
+private extension Logger.OutputConfiguration.TimestampLoggingFormat {
+
+    static let cachedISODateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions.insert(.withFractionalSeconds)
+        return formatter
+    }()
+
+    func render(at date: Date) -> String {
+        switch self {
+            case .highPrecisionISO8601:
+                return Self.cachedISODateFormatter.string(from: date)
+            case .minimal:
+                /// - TODO: Implement. For now just use the ISO 8601 format
+                return Self.cachedISODateFormatter.string(from: date)
+            case .custom(let callback):
+                return callback(date)
         }
     }
 }
