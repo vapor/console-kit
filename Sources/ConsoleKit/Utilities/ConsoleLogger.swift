adda4ab89b94a0f08da6e4a194dcce7b5517f10e
cole <cole@swift-mail.com>
2023-08-12 16:56:00 -0500

    Implement LoggerFragment system for customizing logger output

--- Sources/ConsoleKit/Utilities/ConsoleLogger.swift
+++ Sources/ConsoleKit/Utilities/ConsoleLogger.swift
@@ -1,5 +1,114 @@
 import Logging
 
+/// splits a path on the /Sources/ folder, returning everything after
+///
+///     "/Users/developer/dev/MyApp/Sources/Run/main.swift"
+///     // becomes
+///     "Run/main.swift"
+///
+func conciseSourcePath(_ path: String) -> String {
+#if compiler(>=5.3)
+	return path
+#else
+	let separator: Substring = path.contains("Sources") ? "Sources" : "Tests"
+	return path.split(separator: "/")
+		.split(separator: separator)
+		.last?
+		.joined(separator: "/") ?? path
+#endif
+}
+
+/// A `LoggerFragment` which implements the default logger message format.
+public func defaultLoggerFragment() -> some LoggerFragment {
+	LabelFragment().maxLevel(.trace)
+		.and(LevelFragment().separated(" ").and(MessageFragment().separated(" ")))
+		.and(MetadataFragment().separated(" "))
+		.and(FileFragment().separated(" ").maxLevel(.debug))
+}
+
+/// Outputs logs to a `Console` via a `LoggerFragment` pipeline.
+public struct ConsoleFragmentLogger<T: LoggerFragment>: LogHandler {
+	public let label: String
+	
+	/// See `LogHandler.metadata`.
+	public var metadata: Logger.Metadata
+	
+	/// See `LogHandler.metadataProvider`.
+	public var metadataProvider: Logger.MetadataProvider?
+	
+	/// See `LogHandler.logLevel`.
+	public var logLevel: Logger.Level
+	
+	/// The conosle that the messages will get logged to.
+	public let console: Console
+	
+	/// The `LoggerFragment` this logger outputs through.
+	public var fragment: T
+	
+	/// Creates a new `ConsoleLogger` instance.
+	///
+	/// - Parameters:
+	///   - fragment: The `LoggerFragment` this logger outputs through.
+	///   - label: Unique identifier for this logger.
+	///   - console: The console to log the messages to.
+	///   - level: The minimum level of message that the logger will output. This defaults to `.debug`, the lowest level.
+	///   - metadata: Extra metadata to log with the message. This defaults to an empty dictionary.
+	public init(fragment: T, label: String, console: Console, level: Logger.Level = .debug, metadata: Logger.Metadata = [:]) {
+		self.fragment = fragment
+		self.label = label
+		self.metadata = metadata
+		self.logLevel = level
+		self.console = console
+	}
+	
+	public init(fragment: T, label: String, console: Console, level: Logger.Level = .debug, metadata: Logger.Metadata = [:], metadataProvider: Logger.MetadataProvider?) {
+		self.fragment = fragment
+		self.label = label
+		self.metadata = metadata
+		self.logLevel = level
+		self.console = console
+		self.metadataProvider = metadataProvider
+	}
+	
+	/// See `LogHandler[metadataKey:]`.
+	///
+	/// This just acts as a getter/setter for the `.metadata` property.
+	public subscript(metadataKey key: String) -> Logger.Metadata.Value? {
+		get { return self.metadata[key] }
+		set { self.metadata[key] = newValue }
+	}
+	
+	/// See `LogHandler.log(level:message:metadata:source:file:function:line:)`.
+	public func log(
+		level: Logger.Level,
+		message: Logger.Message,
+		metadata: Logger.Metadata?,
+		source: String,
+		file: String,
+		function: String,
+		line: UInt
+	) {
+		var output = FragmentOutput()
+		var record = LogRecord(
+			level: level,
+			message: message,
+			metadata: metadata,
+			source: source,
+			file: file,
+			function: function,
+			line: line,
+			label: self.label,
+			loggerLevel: self.logLevel,
+			loggerMetadata: self.metadata,
+			metadataProvider: self.metadataProvider
+		)
+		
+		self.fragment.write(&record, to: &output)
+
+		self.console.output(output.text)
+	}
+}
+
 /// Outputs logs to a `Console`.
 public struct ConsoleLogger: LogHandler {
     public let label: String
@@ -15,6 +124,8 @@ public struct ConsoleLogger: LogHandler {
     
     /// The conosle that the messages will get logged to.
     public let console: Console
+	
+	public var fragment: some LoggerFragment = defaultLoggerFragment()
 
     /// Creates a new `ConsoleLogger` instance.
     ///
@@ -46,66 +157,35 @@ public struct ConsoleLogger: LogHandler {
         set { self.metadata[key] = newValue }
     }
     
-    /// See `LogHandler.log(level:message:metadata:file:function:line:)`.
+    /// See `LogHandler.log(level:message:metadata:source:file:function:line:)`.
     public func log(
         level: Logger.Level,
         message: Logger.Message,
         metadata: Logger.Metadata?,
+		source: String,
         file: String,
         function: String,
         line: UInt
     ) {
-        var text: ConsoleText = ""
-        
-        if self.logLevel <= .trace {
-            text += "[ \(self.label) ] ".consoleText()
-        }
-            
-        text += "[ \(level.name) ]".consoleText(level.style)
-            + " "
-            + message.description.consoleText()
+        var output = FragmentOutput()
 
-        let allMetadata = (metadata ?? [:])
-            .merging(self.metadata, uniquingKeysWith: { (a, _) in a })
-            .merging(self.metadataProvider?.get() ?? [:], uniquingKeysWith: { (a, _) in a })
+		var record = LogRecord(
+			level: level,
+			message: message,
+			metadata: metadata,
+			source: source,
+			file: file,
+			function: function,
+			line: line,
+			label: self.label,
+			loggerLevel: self.logLevel,
+			loggerMetadata: self.metadata,
+			metadataProvider: self.metadataProvider
+		)
+		
+		self.fragment.write(&record, to: &output)
 
-        if !allMetadata.isEmpty {
-            // only log metadata if not empty
-            text += " " + allMetadata.sortedDescriptionWithoutQuotes.consoleText()
-        }
-
-        // log file info if we are debug or lower
-        if self.logLevel <= .debug {
-            // log the concise path + line
-            let fileInfo = self.conciseSourcePath(file) + ":" + line.description
-            text += " (" + fileInfo.consoleText() + ")"
-        }
-
-        self.console.output(text)
-    }
-    
-    /// splits a path on the /Sources/ folder, returning everything after
-    ///
-    ///     "/Users/developer/dev/MyApp/Sources/Run/main.swift"
-    ///     // becomes
-    ///     "Run/main.swift"
-    ///
-    private func conciseSourcePath(_ path: String) -> String {
-        let separator: Substring = path.contains("Sources") ? "Sources" : "Tests"
-        return path.split(separator: "/")
-            .split(separator: separator)
-            .last?
-            .joined(separator: "/") ?? path
-    }
-}
-
-private extension Logger.Metadata {
-    var sortedDescriptionWithoutQuotes: String {
-        let contents = Array(self)
-            .sorted(by: { $0.0 < $1.0 })
-            .map { "\($0.description): \($1)" }
-            .joined(separator: ", ")
-        return "[\(contents)]"
+		self.console.output(output.text)
     }
 }
 
---
+++ Sources/ConsoleKit/Utilities/LoggerFragment.swift
@@ -0,0 +1,281 @@
+import Logging
+
+/// Information about a specific log message, including information from the logger the message was logged to.
+public struct LogRecord {
+	/// The log level of the message
+	public var level: Logger.Level
+	/// The logged message
+	public var message: Logger.Message
+	/// The metadata explicitly associated with the logged message
+	public var metadata: Logger.Metadata?
+	/// The source of the log message, usually the module name
+	public var source: String
+	/// The file the message was logged from
+	public var file: String
+	/// The function the message was logged from
+	public var function: String
+	/// The line number in the file the message was logged from
+	public var line: UInt
+	
+	
+	/// The label of the logger the message was logged to
+	public var label: String
+	/// The log level of the logger the message was logged to
+	public var loggerLevel: Logger.Level
+	/// The metadata associated with the logger the message was logged to
+	public var loggerMetadata: Logger.Metadata
+	/// The metadata provider associated with the logger the message was logged to
+	public var metadataProvider: Logger.MetadataProvider?
+	
+	/// The resolved metadata, combining all of the sources. This is computed lazily.
+	var _allMetadata: [String: Logger.MetadataValue]? = nil
+	
+	/// splits a path on the /Sources/ folder, returning everything after
+	///
+	///     "/Users/developer/dev/MyApp/Sources/Run/main.swift"
+	///     // becomes
+	///     "Run/main.swift"
+	///
+	public var conciseSourcePath: String {
+		ConsoleKit.conciseSourcePath(self.file)
+	}
+	
+	/// Combine all of the metadata into a single set. The result is cached after it is computed once.
+	public mutating func allMetadata() -> [String: Logger.MetadataValue] {
+		if let all = self._allMetadata {
+			return all
+		} else {
+			let meta = (self.metadata ?? [:])
+				.merging(self.loggerMetadata, uniquingKeysWith: { (a, _) in a })
+				.merging(self.metadataProvider?.get() ?? [:], uniquingKeysWith: { (a, _) in a })
+			self._allMetadata = meta
+			return meta
+		}
+	}
+}
+
+/// The output of a `LoggerFragment`, including some intermediary state used for things like deduplicating separators.
+public struct FragmentOutput {
+	public var text = ConsoleText()
+	public var needsSeparator = false
+	
+	public init() { }
+
+	public static func +=(lhs: inout FragmentOutput, rhs: ConsoleText) {
+		lhs.text = ConsoleText(fragments: lhs.text.fragments + rhs.fragments)
+	}
+}
+
+/// A fragment of a log message.
+public protocol LoggerFragment {
+	/// Indicates whether the fragment will write anything to `output` when `write` is called. This is used to determine whether writing a separator should be skipped.
+	func hasContent(record: inout LogRecord) -> Bool
+	
+	/// Add this fragment's output to the console text.
+	///
+	/// Fragments are allowed to mutate the `LogRecord` seen by later fragments in the pipeline, but this should generally be done before any fragments write text to avoid inconsistencies in the final message.
+	func write(_ record: inout LogRecord, to output: inout FragmentOutput)
+}
+
+extension LoggerFragment {
+	public func hasContent(record: inout LogRecord) -> Bool {
+		true
+	}
+}
+
+extension LoggerFragment {
+	/// Make the current fragment conditional, only calling its `output` method if the record's `loggerLevel` is `maxLevel` or lower
+	///
+	///	The sequence
+	/// ```
+	/// Literal("IsDebugOrTrace").maxLevel(.debug)
+	/// ```
+	///	will only include "IsDebugOrTrace" in the output when the log level is debug or lower.
+	func maxLevel(_ level: Logger.Level) -> IfMaxLevelFragment<Self> {
+		IfMaxLevelFragment(self, maxLevel: level)
+	}
+	
+	/// Combine the current fragment with another, which will be written after the current fragment finishes.
+	func and<T: LoggerFragment>(_ other: T) -> AndFragment<Self, T> {
+		AndFragment(self, other)
+	}
+	
+	/// Add a literal prefix to the current fragment.
+	func prefixed(_ text: ConsoleText) -> AndFragment<LiteralFragment, Self> {
+		AndFragment(LiteralFragment(text), self)
+	}
+	
+	/// Add a literal suffix to the current fragment.
+	func suffixed(_ text: ConsoleText) -> AndFragment<Self, LiteralFragment> {
+		AndFragment(self, LiteralFragment(text))
+	}
+	
+	/// Appends the given separator text to the output before `self`'s output, as long as a separator is needed.
+	///
+	/// If the wrapped fragment reports that it has no content, no separator will be inserted.
+	func separated(_ text: ConsoleText) -> SeparatorFragment<Self> {
+		SeparatorFragment(text, fragment: self)
+	}
+}
+
+/// Make the current fragment conditional, only calling its `output` method if the record's `loggerLevel` is `maxLevel` or lower
+///
+///	The sequence
+/// ```
+/// Literal("IsDebugOrTrace").maxLevel(.debug)
+/// ```
+///	will only include "IsDebugOrTrace" in the output when the log level is debug or lower.
+///
+///	This fragment is considered to not have content if the logging level is higher than than `maxLevel`.
+public struct IfMaxLevelFragment<T: LoggerFragment>: LoggerFragment {
+	public let maxLevel: Logger.Level
+	public let fragment: T
+	
+	public init(_ fragment: T, maxLevel: Logger.Level) {
+		self.fragment = fragment
+		self.maxLevel = maxLevel
+	}
+	
+	func shouldWrite(_ record: LogRecord) -> Bool {
+		record.loggerLevel <= self.maxLevel
+	}
+	
+	public func hasContent(record: inout LogRecord) -> Bool {
+		if self.shouldWrite(record) {
+			return self.fragment.hasContent(record: &record)
+		} else {
+			return false
+		}
+	}
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		if self.shouldWrite(record) {
+			self.fragment.write(&record, to: &output)
+		}
+	}
+}
+
+/// Combine the current fragment with another, which will be written after the current fragment finishes.
+public struct AndFragment<T: LoggerFragment, U: LoggerFragment>: LoggerFragment {
+	public let first: T
+	public let second: U
+	
+	public init(_ first: T, _ second: U) {
+		self.first = first
+		self.second = second
+	}
+	
+	public func hasContent(record: inout LogRecord) -> Bool {
+		self.first.hasContent(record: &record) || self.second.hasContent(record: &record)
+	}
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		self.first.write(&record, to: &output)
+		self.second.write(&record, to: &output)
+	}
+}
+
+/// Writes the label of the logger, and requests a separator for the next fragment.
+public struct LabelFragment: LoggerFragment {
+	public init() { }
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		output += "[ \(record.label) ]".consoleText()
+		output.needsSeparator = true
+	}
+}
+
+/// Writes the level of the logged message, and requests a separator for the next fragment.
+public struct LevelFragment: LoggerFragment {
+	public init() { }
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		output += "[ \(record.level.name) ]".consoleText(record.level.style)
+		output.needsSeparator = true
+	}
+}
+
+/// Writes the given text to the output. This type does not request a separator for the next fragment
+public struct LiteralFragment: LoggerFragment {
+	public let literal: ConsoleText
+	
+	public init(_ literal: ConsoleText) {
+		self.literal = literal
+	}
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		output += self.literal
+	}
+}
+
+public struct SeparatorFragment<T: LoggerFragment>: LoggerFragment {
+	public let literal: ConsoleText
+	public var fragment: T
+	
+	public init(_ literal: ConsoleText, fragment: T) {
+		self.literal = literal
+		self.fragment = fragment
+	}
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		if output.needsSeparator {
+			if self.fragment.hasContent(record: &record) {
+				output.needsSeparator = false
+				output += self.literal
+			}
+		}
+		
+		self.fragment.write(&record, to: &output)
+	}
+}
+
+/// Writes the logged message to the output, and requests a separator for the next fragment.
+public struct MessageFragment: LoggerFragment {
+	public init() { }
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		output += record.message.description.consoleText()
+		output.needsSeparator = true
+	}
+}
+
+/// Writes the combined metadata to the output, and requests a separator for the next fragment only if the metadata was not empty.
+///
+/// This fragment is considered to not have content if the metadata is empty.
+public struct MetadataFragment: LoggerFragment {
+	public init() { }
+	
+	public func hasContent(record: inout LogRecord) -> Bool {
+		 !record.allMetadata().isEmpty
+	}
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		let allMetadata = record.allMetadata()
+		
+		guard !allMetadata.isEmpty else { return }
+		
+		output += allMetadata.sortedDescriptionWithoutQuotes.consoleText()
+		output.needsSeparator = true
+	}
+}
+
+/// Writes the file location of the logged message, including the line. This fragment requests a separator for the next fragment.
+public struct FileFragment: LoggerFragment {
+	public init() { }
+	
+	public func write(_ record: inout LogRecord, to output: inout FragmentOutput) {
+		let file = record.conciseSourcePath + ":" + record.line.description
+		output += "(" + file.consoleText() + ")"
+		output.needsSeparator = true
+	}
+}
+
+private extension Logger.Metadata {
+	var sortedDescriptionWithoutQuotes: String {
+		let contents = Array(self)
+			.sorted(by: { $0.0 < $1.0 })
+			.map { "\($0.description): \($1)" }
+			.joined(separator: ", ")
+		return "[\(contents)]"
+	}
+}
