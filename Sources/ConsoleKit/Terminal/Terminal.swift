cc584bd849ef3f8c258f4f18bfcc3d5a80876d6d
Tim Condon <0xTim@users.noreply.github.com>
2023-11-04 15:39:54 +0000

    Improve AnySendableHashable to match Hashable (#191)

--- Sources/ConsoleKit/Terminal/Terminal.swift
+++ Sources/ConsoleKit/Terminal/Terminal.swift
@@ -8,20 +8,6 @@ import CRT
 import Foundation
 import NIOConcurrencyHelpers
 
-/// A `Sendable` version of the standard library's `AnyHashable` type.
-public struct AnySendableHashable: @unchecked Sendable, Hashable, ExpressibleByStringLiteral {
-    // Note: @unchecked Sendable since there's no way to express that `wrappedValue` is Sendable, even though we ensure that it is in the init.
-    let wrappedValue: AnyHashable
-    
-    public init(_ wrappedValue: any Hashable & Sendable) {
-        self.wrappedValue = AnyHashable(wrappedValue)
-    }
-    
-    public init(stringLiteral value: String) {
-        self.init(value)
-    }
-}
-
 /// Generic console that uses a mixture of Swift standard
 /// library and Foundation code to fulfill protocol requirements.
 public final class Terminal: Console, Sendable {
