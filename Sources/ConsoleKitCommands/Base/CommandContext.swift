18262d24c98c4b76aed2d6def3544b58d0cbad63
Marius Seufzer <44228394+marius-se@users.noreply.github.com>
2023-12-02 07:21:28 +1300

    Add merge function to command groups (#195)
    * add merge function to command groups
    
    * Fix compilation error
    
    ---------
    
    Co-authored-by: Tim <0xtimc@gmail.com>

--- Sources/ConsoleKitCommands/Base/CommandContext.swift
+++ Sources/ConsoleKitCommands/Base/CommandContext.swift
@@ -1,15 +1,16 @@
 import protocol ConsoleKitTerminal.Console
+import struct ConsoleKitTerminal.AnySendableHashable
 
 /// A type-erased `CommandContext`
-public struct CommandContext {
+public struct CommandContext: Sendable {
     /// The `Console` this command was run on.
     public var console: any Console
-    
+
     /// The parsed arguments (according to declared signature).
     public var input: CommandInput
 
-    public var userInfo: [AnyHashable: Any]
-    
+    public var userInfo: [AnySendableHashable: any Sendable]
+
     /// Create a new `AnyCommandContext`.
     public init(
         console: any Console,
