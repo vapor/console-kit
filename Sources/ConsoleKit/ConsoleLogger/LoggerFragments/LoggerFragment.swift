5afd495f234417f7d19d621fe6200ed6c72a6a94
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2025-09-01 17:46:04 +0200

    Move `LogRecord` to its own file

--- Sources/ConsoleKit/ConsoleLogger/LoggerFragments/LoggerFragment.swift
+++ Sources/ConsoleKit/ConsoleLogger/LoggerFragments/LoggerFragment.swift
@@ -15,69 +15,6 @@ public import WASILibc
 public import CRT
 #endif
 
-/// Information about a specific log message, including information from the logger the message was logged to.
-public struct LogRecord {
-    public init(
-        level: Logger.Level,
-        message: Logger.Message,
-        metadata: Logger.Metadata? = nil,
-        source: String,
-        file: String,
-        function: String,
-        line: UInt,
-        label: String,
-        loggerLevel: Logger.Level,
-        loggerMetadata: Logger.Metadata,
-        metadataProvider: Logger.MetadataProvider? = nil
-    ) {
-        self.level = level
-        self.message = message
-        self.metadata = metadata
-        self.source = source
-        self.file = file
-        self.function = function
-        self.line = line
-        self.label = label
-        self.loggerLevel = loggerLevel
-        self.loggerMetadata = loggerMetadata
-        self.metadataProvider = metadataProvider
-    }
-
-    /// The log level of the message
-    public var level: Logger.Level
-    /// The logged message
-    public var message: Logger.Message
-    /// The metadata explicitly associated with the logged message
-    public var metadata: Logger.Metadata?
-    /// The source of the log message, usually the module name
-    public var source: String
-    /// The file the message was logged from
-    public var file: String
-    /// The function the message was logged from
-    public var function: String
-    /// The line number in the file the message was logged from
-    public var line: UInt
-
-    /// The label of the logger the message was logged to
-    public var label: String
-    /// The log level of the logger the message was logged to
-    public var loggerLevel: Logger.Level
-    /// The metadata associated with the logger the message was logged to
-    public var loggerMetadata: Logger.Metadata
-    /// The metadata provider associated with the logger the message was logged to
-    public var metadataProvider: Logger.MetadataProvider?
-
-    /// Combine all of the metadata into a single set.
-    public mutating func allMetadata() -> [String: Logger.MetadataValue] {
-        // We aren't mutating self here currently,
-        // but keeping the method marked that way will ensure we can cache the result
-        // without breaking the public API if we decide that's desirable.
-        (self.metadata ?? [:])
-            .merging(self.loggerMetadata, uniquingKeysWith: { (a, _) in a })
-            .merging(self.metadataProvider?.get() ?? [:], uniquingKeysWith: { (a, _) in a })
-    }
-}
-
 /// The output of a ``LoggerFragment``, including some intermediary state used for things like deduplicating separators.
 public struct FragmentOutput {
     public var text = ConsoleText()
