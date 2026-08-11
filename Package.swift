27a3a13fcbc16295f7010d1c119a1c7990291aa7
Tim Condon <0xTim@users.noreply.github.com>
2026-08-11 12:45:17 +0100

    Don't Use Swift Configuration Default Traits (#241)
    Don't pull in the default traits

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.11.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.1.1"),
-        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0", traits: [.defaults, "CommandLineArguments"]),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0", traits: ["CommandLineArguments"]),
     ],
     targets: [
         .target(
